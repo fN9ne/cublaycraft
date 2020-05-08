e99bf4f9c449b6560469b408534feabfc1df6ac7
fN9ne <39432487+fN9ne@users.noreply.github.com>
2020-05-08 16:24:51 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,40 @@
+var video = document.querySelector('.video');
+var info = document.querySelector('.page__information');
+var pv = document.querySelector('.page__video');
+
+function togglePlayPause() {
+	if(video.paused) {
+		video.style.width = '1280px';
+		video.style.height = '720px';
+		pv.style.width = '1280px';
+		pv.style.height = '720px';
+		video.setAttribute('controls','')
+		info.style.right = '0';
+		pv.style.bottom = '15px';
+		pv.style.right = '320px';
+	} else {
+		video.style.width = '310px';
+		video.style.height = '200px';
+		pv.style.width = '310px';
+		pv.style.height = '200px';
+		info.style.right = '310px';
+		pv.style.bottom = '143px';
+		pv.style.right = '770px';
+	}
+}
+
+video.onclick = function() {
+	togglePlayPause();
+};
+
+$(document).ready(function(){
+	$('.page__slider__images').slick({
+		infinite: true,
+  		speed: 1000,
+  		slidesToShow: 2,
+  		variableWidth: true,
+ 		autoplay: true,
+  		autoplaySpeed: 5000,
+  		pasueOnHover: true,
+	});
+});
